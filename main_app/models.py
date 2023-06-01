9482ca5f399f848bbb39b44dc5a6f21b888c87ee
dangabrielle <hoangabrielle@gmail.com>
2023-06-01 13:31:54 -1000

    add new Feeding model, create migration

--- main_app/models.py
+++ main_app/models.py
@@ -15,3 +15,19 @@ class Finch(models.Model):
 
     def get_absolute_url(self):  # redirects back to the detail page after creating a cat
         return reverse('detail', kwargs={'finch_id': self.id})
+
+
+class Feeding(models.Model):
+    MEALS = (
+        ('B', 'Breakfast'),
+        ('L', 'Lunch'),
+        ('D', 'Dinner')
+    )
+    date = models.DateField()
+    meal = models.CharField(max_length=1, choices=MEALS, default=MEALS[0][0])
+    # Create a cat_id FK
+    finch = models.ForeignKey(Finch, on_delete=models.CASCADE)
+
+    def __str__(self):
+        # Nice method for obtaining the friendly value of a Field.choice
+        return f"{self.get_meal_display()} on {self.date}"
