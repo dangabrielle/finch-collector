09dd64b42cb679a023f80896b28ea45a1b22d0df
dangabrielle <hoangabrielle@gmail.com>
2023-06-03 21:08:56 -1000

    complete pt 5

--- main_app/views.py
+++ main_app/views.py
@@ -1,7 +1,8 @@
 from django.shortcuts import render, redirect
-from .models import Finch
+from .models import Finch, Toy
 from django.views.generic.edit import CreateView, UpdateView, DeleteView
 from .forms import FeedingForm
+from django.views.generic import ListView, DetailView
 # Create your views here.
 
 
@@ -25,8 +26,13 @@ def finch_index(request):
 
 def finch_detail(request, finch_id):
     finch = Finch.objects.get(id=finch_id)
+    # First, create a list of the toy ids that the cat DOES have
+    id_list = finch.toys.all().values_list('id', flat=True)
+  # Now we can query for toys whose ids are not in the list using exclude
+    toys_finch_doesnt_have = Toy.objects.exclude(id__in=id_list)
     feeding_form = FeedingForm()
-    return render(request, 'finches/details.html', {'finch': finch, 'feeding_form': feeding_form})
+
+    return render(request, 'finches/details.html', {'finch': finch, 'feeding_form': feeding_form, 'toys': toys_finch_doesnt_have})
 
 
 def add_feeding(request, finch_id):
@@ -39,7 +45,7 @@ def add_feeding(request, finch_id):
         new_feeding = form.save(commit=False)
         new_feeding.finch_id = finch_id
         new_feeding.save()
-    return redirect('detail', finch_id=finch_id)
+    return redirect('details', finch_id=finch_id)
 
 
 class FinchCreate(CreateView):
@@ -57,3 +63,38 @@ class FinchUpdate(UpdateView):
 class FinchDelete(DeleteView):
     model = Finch
     success_url = '/finches/'
+
+
+class ToyCreate(CreateView):
+    model = Toy
+    fields = ['name', 'color']
+
+
+class ToyList(ListView):
+    model = Toy
+
+
+class ToyDetail(DetailView):
+    model = Toy
+
+
+class ToyUpdate(UpdateView):
+    model = Toy
+    fields = ['name', 'color']
+
+
+class ToyDelete(DeleteView):
+    model = Toy
+    success_url = '/toys/'
+
+
+def assoc_toy(request, finch_id, toy_id):
+  # Note that you can pass a toy's id instead of the whole toy object
+    Finch.objects.get(id=finch_id).toys.add(toy_id)
+    return redirect('detail', finch_id=finch_id)
+
+
+def unassoc_toy(request, finch_id, toy_id):
+    # Note that you can pass a toy's id instead of the whole toy object
+    Finch.objects.get(id=finch_id).toys.remove(toy_id)
+    return redirect('detail', finch_id=finch_id)
